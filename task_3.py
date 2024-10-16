568d543be1330411cd909894f63a6246fa15c6da
dima cheban <dimacheban23@gmail.com>
2024-10-16 12:51:39 +0300

    update function, added check extra '+'

--- task_3.py
+++ task_3.py
@@ -3,12 +3,18 @@ import re
 
 def normalize_phone(phone_number: str) -> str:
     normalized = re.sub(r"[^0-9+]", "", phone_number).strip()
+
+    if normalized.count('+') > 1:
+        normalized = normalized[0] + normalized[1:].replace('+', '')
+    
     if normalized.startswith("0"):
         normalized = "+38" + normalized
     elif normalized.startswith("380"):
         normalized = "+" + normalized
     elif normalized.isdigit():
         normalized = "+" + normalized
+
     if len(normalized) != 13:
-        normalized = "error number format"
+        normalized = f"error format"
+
     return normalized
