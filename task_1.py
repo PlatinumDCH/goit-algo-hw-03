f6c2febe36cd086ec9588bbc77d25f589f80f664
dima cheban <dimacheban23@gmail.com>
2024-10-16 19:16:30 +0300

    modift input forat, correting tests[work with uncorrect input data]

--- task_1.py
+++ task_1.py
@@ -2,17 +2,20 @@ from datetime import datetime
 
 
 def string_to_date(date_string):
-    """Converts a date string in the format YYYY.MM.DD to a date object."""
+    """Converts a date string in the format YYYY-MM-DD to a date object."""
     try:
-        return datetime.strptime(date_string, "%Y.%m.%d").date()
+        return datetime.strptime(date_string, "%Y-%m-%d").date()
     except ValueError:
-        raise ValueError("Incorrect date format, should be YYYY.MM.DD")
+        return None
 
 
 
 def get_days_from_today(date_str: str):
+    date_obj = string_to_date(date_str)
+    if date_obj is None:
+        return 'Invalid date format. Expected [YYYY-MM-DD]'
+    
     current_day = datetime.today().date()
-    data_obj = string_to_date(date_str)
-    return (current_day - data_obj).days
+    return (current_day - date_obj).days
 
-print(get_days_from_today("2025.01.01"))
+
--- test_task_1.py
+++ test_task_1.py
@@ -6,26 +6,27 @@ class TestGetDaysFromToday(unittest.TestCase):
     
     def test_past_date(self):
         '''past date'''
-        self.assertGreater(get_days_from_today("2022.01.01"), 0)
+        self.assertGreater(get_days_from_today("2022-01-01"), 0)
     
     def test_future_date(self):
         '''future date'''
-        self.assertLess(get_days_from_today("2025.01.01"), 0)
+        self.assertLess(get_days_from_today("2025-01-01"), 0)
     
     def test_today_date(self):
         '''current date'''
-        today = datetime.today().strftime("%Y.%m.%d")
+        today = datetime.today().strftime("%Y-%m-%d")
         self.assertEqual(get_days_from_today(today), 0)
     
     def test_incorrect_format(self):
         '''uncorrect format'''
-        with self.assertRaises(ValueError):
-            get_days_from_today("2025-01-01")
+        message = "Invalid date format. Expected [YYYY-MM-DD]"
+        self.assertEqual(get_days_from_today("2025/01/01"),message)
+        self.assertEqual(get_days_from_today("2025-01-01T00:00:00"),message)
 
     def test_invalid_date(self):
         '''non-existent date'''
-        with self.assertRaises(ValueError):
-            get_days_from_today("2023.02.30")
+        message = "Invalid date format. Expected [YYYY-MM-DD]"
+        self.assertEqual(get_days_from_today("2023-02-30"), message)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
